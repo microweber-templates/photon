c091e1440d0f6c267e5459f3161587bcb47523f5
Petko Yovchevski <pyovchevski@gmail.com>
2018-03-06 17:03:10 +0200

    updates

---
+++ layouts.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+
+  type: layout
+  content_type: static
+  name: Layouts
+  position: 11
+  description: Home layout
+
+*/
+
+?>
+<?php include template_dir() . "header.php"; ?>
+
+<div class="edit" rel="content" field="photon_content">
+    <module type="layouts" template="skin-1"/>
+    <module type="layouts" template="skin-2"/>
+    <module type="layouts" template="skin-3"/>
+    <module type="layouts" template="skin-4"/>
+    <module type="layouts" template="skin-5"/>
+    <module type="layouts" template="skin-6"/>
+    <module type="layouts" template="skin-7"/>
+</div>
+
+<?php include template_dir() . "footer.php"; ?>
