3ac4c55be910c48c97fd5b6239a80a2650b853a1
Petko Yovchevski <pyovchevski@gmail.com>
2019-05-30 10:44:00 +0300

    Incrementing version number to 1.1

--- config.php
+++ config.php
@@ -4,4 +4,5 @@ $config = array();
 $config['name'] = "Photon";
 $config['description'] = "Photo website template";
 $config['author'] = "Microweber";
-$config['version'] = 1.0;
+$config['version'] = 1.1;
+$config['standalone_module_skins'] = true;
